c26778125b8b04fbe8c96a706534dab135eb9671
Anastasiia_Dremina <anastasyadremina@rambler.ru>
2018-07-11 10:39:07 +0300

    Homework 4. Updated

--- src/main/java/com/epam/mobile/setup/TestProperties.java
+++ src/main/java/com/epam/mobile/setup/TestProperties.java
@@ -9,16 +9,17 @@ public class TestProperties {
     private static Properties currentProps;
 
     Properties getCurrentProps() throws IOException {
-        return (currentProps != null) ? currentProps : readPropertyFromFile();
+        return (currentProps != null) ? currentProps : readPropertiesFromFile();
     }
 
     public String getProp(String propKey) throws IOException {
         return getCurrentProps().getProperty(propKey, null);
     }
 
-    private Properties readPropertyFromFile() throws IOException {
-        this.currentProps = new Properties();
+    private Properties readPropertiesFromFile() throws IOException {
+
         FileInputStream in = new FileInputStream(System.getProperty("property"));
+        this.currentProps = new Properties();
         this.currentProps.load(in);
         in.close();
         return this.currentProps;
